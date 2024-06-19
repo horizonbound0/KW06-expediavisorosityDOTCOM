5ea1fefe188138449d655a9a251621efd90e95c2
Kenneth A Wagner <kenw2190@gmail.com>
2024-06-19 11:49:03 -0500

    forgot to switch branches

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/*
+5 day 3 hr forecast API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+coords by location name: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+reverse geocoding: http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
+*/
+
+let requestURL;
