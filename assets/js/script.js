1749827e9dc26bde9f68be97284cbee555a89b21
Kenneth A Wagner <kenw2190@gmail.com>
2024-06-20 21:40:38 -0500

    made basic HTML structure in preparation for testing requested info to be displayed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,44 @@
 /*
-5 day 3 hr forecast API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+5 day 3 hr forecast API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=89c2d10cea5bf468636c45b15924d79d
 
-coords by location name: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-reverse geocoding: http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
+coords by location name: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=89c2d10cea5bf468636c45b15924d79d
+
+
+reverse geocoding: http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid=89c2d10cea5bf468636c45b15924d79d
+
 */
 
-let requestURL;
+let locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=89c2d10cea5bf468636c45b15924d79d";
+
+function getLocalStorageLocNames () {
+    if(localStorage.getItem('Location Names') != null) {
+        localStorage.getItem('Location Names');
+    }
+}
+
+function getAPI() {
+    fetch(locationURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            console.log(data);
+
+            const locationResults = [];
+
+            for (let location of data) {
+                
+                const locationName = location.name;
+                console.log(`Location name: ${locationName}`);
+                locationResults.push(locationName);
+
+            }
+            
+            console.log(`Location names: ${locationResults}`);
+
+        })
+}
+
+getAPI();
