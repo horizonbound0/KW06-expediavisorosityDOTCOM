43de30d797287702c31b33a151e017506bf0e90f
Kenneth A Wagner <kenw2190@gmail.com>
2024-06-22 12:32:49 -0500

    was able to call 5 results from a user prompted search. Next I'm adding a simple JSON file with all country codes and using it to populate a dropdown menu for the user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,36 +9,102 @@ reverse geocoding: http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={
 
 */
 
-let locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=89c2d10cea5bf468636c45b15924d79d";
+let locationURL = ``;
 
-function getLocalStorageLocNames () {
-    if(localStorage.getItem('Location Names') != null) {
-        localStorage.getItem('Location Names');
+const searchFormEl = $('#search-form');
+const searchInputEl = $('#search');
+const modalBodyEl = $('.modal-body');
+
+let userSearchVal = '';
+
+// making a function to store an array into local storage
+function setSearchHistory(someArray) {
+    localStorage.setItem('results', JSON.stringify(someArray));
+}
+
+// making a function to return local storage, or make an empty array.
+function getSearchHistory() {
+    let results = [];
+
+    if (localStorage.getItem('results') != null) {
+        results = JSON.parse(localStorage.getItem('results'));
+        return results;
+    } else {
+
+        return results;
+
     }
 }
 
-function getAPI() {
+// making a function to grab search results
+function handleSearch(event) {
+
+    event.preventDefault();
+    const target = event.target;
+    const targetSearch = target.dataset.search;
+
+    if (targetSearch === 'search-button') {
+        let userSearchVal = '';
+        const apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+        const apiKey = '&limit=5&appid=89c2d10cea5bf468636c45b15924d79d';
+
+        userSearchVal = searchInputEl.val();
+        locationURL = `${apiURL}${userSearchVal}${apiKey}`;
+        getLocationAPI();
+    }
+}
+
+searchFormEl.on('click', handleSearch);
+
+// create a function to make a card for each result
+function createResultsCard(result) {
+
+}
+
+
+function getLocationAPI() {
+
     fetch(locationURL)
         .then(function (response) {
+
             return response.json();
+
         })
         .then(function (data) {
 
             console.log(data);
 
-            const locationResults = [];
-
             for (let location of data) {
-                
-                const locationName = location.name;
-                console.log(`Location name: ${locationName}`);
-                locationResults.push(locationName);
+
+                // declaring the location object
+                let searchResult = {
+                    name: '',
+                    state: '',
+                    country: '',
+                    lat: '',
+                    lon: ''
+                }
+
+                // get the locations array
+                let results = getSearchHistory();
+
+                searchResult.name = location.name;
+                searchResult.state = location.state;
+                searchResult.country = location.country;
+                searchResult.lat = location.lat;
+                searchResult.lon = location.lon;
+
+                results.push(searchResult);
+
+                setSearchHistory(results);
 
             }
-            
-            console.log(`Location names: ${locationResults}`);
 
         })
 }
 
-getAPI();
+//console.log(`Locations: ${getSearchHistory}`);
+            
+//console.log(typeof getSearchHistory);
+
+//getAPI();
