7b5ae5fe573b1b9011728a8e6bfd6ceb2da3edff
Kenneth A Wagner <kenw2190@gmail.com>
2024-06-22 15:37:03 -0500

    Got the search history section working, and tweaked the location API fetch a bit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /*
-5 day 3 hr forecast API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=89c2d10cea5bf468636c45b15924d79d
+5 day 3 hr forecast API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=89c2d10cea5bf468636c45b15924d79d&units=imperial
 
 
 coords by location name: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=89c2d10cea5bf468636c45b15924d79d
@@ -10,12 +10,14 @@ reverse geocoding: http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={
 */
 
 let locationURL = ``;
+let weatherURL = ``;
+
+let lastSearch = ``;
 
 const searchFormEl = $('#search-form');
 const searchInputEl = $('#search');
-const modalBodyEl = $('.modal-body');
-
-let userSearchVal = '';
+const resultsEl = $('#results');
+const searchHistoryEl = $('#search-history');
 
 // making a function to store an array into local storage
 function setSearchHistory(someArray) {
@@ -41,34 +43,90 @@ function handleSearch(event) {
 
     event.preventDefault();
     const target = event.target;
-    const targetSearch = target.dataset.search;
+    const targetSearch = target.dataset.type;
+
+    const apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+    const apiKey = '&limit=1&appid=89c2d10cea5bf468636c45b15924d79d';
 
     if (targetSearch === 'search-button') {
-        let userSearchVal = '';
-        const apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
-        const apiKey = '&limit=5&appid=89c2d10cea5bf468636c45b15924d79d';
 
-        userSearchVal = searchInputEl.val();
+        let userSearchVal = searchInputEl.val();
+
         locationURL = `${apiURL}${userSearchVal}${apiKey}`;
+
         getLocationAPI();
+
+    } else if (targetSearch === 'history') {
+
+        let userSearchVal = target.dataset.name;
+
+        locationURL = `${apiURL}${userSearchVal}${apiKey}`;
+
+        getLocationAPI();
+
     }
 }
 
 searchFormEl.on('click', handleSearch);
+printSearchHistory();
+
+// function to populate the search history section
+function printSearchHistory() {
+    const cityList = getSearchHistory();
+
+    for (place of cityList) {
+        const historyButton = $('<button>')
+            .attr('data-type', 'history')
+            .attr('data-name', place.name)
+            .text(place.name);
+
+        searchHistoryEl.append(historyButton);
+    }
+}
 
 // create a function to make a card for each result
-function createResultsCard(result) {
+function createResultsCard(city) {
+
+    const cityCard = $('<div>')
+        .addClass('card my-3')
+        .attr('data-city', city.name);
+
+    const cityName = $('<h2>')
+        .addClass('card-title h2')
+        .text(city.name);
+
+    const cityBody = $('<div>')
+        .addClass('card-body');
+
+    const cityTemp = $('<h3>')
+        .addClass('card-text')
+        .text(city.weather.temp);
+
+    const cityWind = $('<h4>')
+        .addClass('card-text')
+        .text(city.weather.wind);
+
+    const cityHumid = $('<h4>')
+        .addClass('card-text')
+        .text(city.weather.humid);
 
 }
 
+function getWeatherAPI() {
+    fetch()
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
 
 function getLocationAPI() {
 
     fetch(locationURL)
         .then(function (response) {
-
             return response.json();
-
         })
         .then(function (data) {
 
@@ -79,8 +137,6 @@ function getLocationAPI() {
                 // declaring the location object
                 let searchResult = {
                     name: '',
-                    state: '',
-                    country: '',
                     lat: '',
                     lon: ''
                 }
@@ -89,22 +145,27 @@ function getLocationAPI() {
                 let results = getSearchHistory();
 
                 searchResult.name = location.name;
-                searchResult.state = location.state;
-                searchResult.country = location.country;
                 searchResult.lat = location.lat;
                 searchResult.lon = location.lon;
+
+                lastSearch = {
+                    lat: location.lat,
+                    lon: location.lon
+                }
+
+                localStorage.setItem('last', JSON.stringify(lastSearch));
 
                 results.push(searchResult);
 
                 setSearchHistory(results);
 
+                searchHistoryEl.empty();
+                searchInputEl.val('');
+                printSearchHistory();
+
+                //getWeatherAPI();
+
             }
 
         })
 }
-
-//console.log(`Locations: ${getSearchHistory}`);
-            
-//console.log(typeof getSearchHistory);
-
-//getAPI();
